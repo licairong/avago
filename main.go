40cfb6ab2474db3c419ea70323f66f8691232af0
licairong <licairong@idcos.com>
2023-03-30 11:35:15 +0800

    test

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
     "os/exec"
 )
 
-const tool = "/opt/MegaRAID/storcli/storcli64"
+//const tool = "/opt/MegaRAID/storcli/storcli64"
+const tool = "ls"
 
 type PluginService struct{}
 
 func (p PluginService) Hello(name string) (string, error) {
     // /opt/MegaRAID/storcli/storcli64 show
-    cmd := exec.Command(tool, "show")
+    cmd := exec.Command(tool, "-l", "/var/log")
     out, err := cmd.CombinedOutput()
     if err != nil {
         return "", err
